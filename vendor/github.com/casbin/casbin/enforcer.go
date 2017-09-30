7015cd63cf49cead6f7c183a6fd07ab2f5cfdebf
Jolly23 <pilot_lei@foxmail.com>
2017-09-30 10:56:43 +0800

    no

--- vendor/github.com/casbin/casbin/enforcer.go
+++ vendor/github.com/casbin/casbin/enforcer.go
@@ -15,12 +15,25 @@
 package casbin
 
 import (
+	"fmt"
+	"reflect"
+
 	"github.com/Knetic/govaluate"
-	"github.com/casbin/casbin/config"
+	"github.com/casbin/casbin/file-adapter"
 	"github.com/casbin/casbin/model"
 	"github.com/casbin/casbin/persist"
-	"log"
-	"reflect"
+	"github.com/casbin/casbin/rbac"
+	"github.com/casbin/casbin/util"
+)
+
+// Effect is the result for a policy rule.
+type Effect int
+
+// Values for policy effect.
+const (
+	EffectAllow Effect = iota
+	EffectIndeterminate
+	EffectDeny
 )
 
 // Enforcer is the main interface for authorization enforcement and policy management.
@@ -28,29 +41,54 @@ type Enforcer struct {
 	modelPath string
 	model     model.Model
 	fm        model.FunctionMap
+	rmc       rbac.RoleManagerConstructor
 
 	adapter persist.Adapter
 
-	enabled bool
+	enabled  bool
+	autoSave bool
 }
 
-// NewEnforcer gets an enforcer via CONF, file or DB.
-// e := NewEnforcer("path/to/casbin.conf")
-// e := NewEnforcer("path/to/basic_model.conf", "path/to/basic_policy.conf")
-// e := NewEnforcer("path/to/rbac_model.conf", "mysql", "root:@tcp(127.0.0.1:3306)/")
+// NewEnforcer creates an enforcer via file or DB.
+// File:
+// e := casbin.NewEnforcer("path/to/basic_model.conf", "path/to/basic_policy.conf")
+// MySQL DB:
+// a := mysqladapter.NewDBAdapter("mysql", "mysql_username:mysql_password@tcp(127.0.0.1:3306)/")
+// e := casbin.NewEnforcer("path/to/basic_model.conf", a)
 func NewEnforcer(params ...interface{}) *Enforcer {
 	e := &Enforcer{}
+	e.rmc = rbac.DefaultRoleManager()
 
-	if len(params) == 1 {
-		e.InitWithConfig(params[0].(string))
-	} else if len(params) == 2 {
-		if reflect.TypeOf(params[1]).Kind() == reflect.String {
-			e.InitWithFile(params[0].(string), params[1].(string))
+	parsedParamLen := 0
+	if len(params) >= 1 && reflect.TypeOf(params[len(params)-1]).Kind() == reflect.Bool {
+		enableLog := params[len(params)-1].(bool)
+		e.EnableLog(enableLog)
+
+		parsedParamLen++
+	}
+
+	if len(params)-parsedParamLen == 2 {
+		if reflect.TypeOf(params[0]).Kind() == reflect.String {
+			if reflect.TypeOf(params[1]).Kind() == reflect.String {
+				e.InitWithFile(params[0].(string), params[1].(string))
+			} else {
+				e.InitWithAdapter(params[0].(string), params[1].(persist.Adapter))
+			}
 		} else {
-			e.InitWithAdapter(params[0].(string), params[1].(persist.Adapter))
+			if reflect.TypeOf(params[1]).Kind() == reflect.String {
+				panic("Invalid parameters for enforcer.")
+			} else {
+				e.InitWithModelAndAdapter(params[0].(model.Model), params[1].(persist.Adapter))
+			}
 		}
-	} else if len(params) == 3 {
-		e.InitWithDB(params[0].(string), params[1].(string), params[2].(string))
+	} else if len(params)-parsedParamLen == 1 {
+		if reflect.TypeOf(params[0]).Kind() == reflect.String {
+			e.InitWithFile(params[0].(string), "")
+		} else {
+			e.InitWithModelAndAdapter(params[0].(model.Model), nil)
+		}
+	} else if len(params)-parsedParamLen == 0 {
+		e.InitWithFile("", "")
 	} else {
 		panic("Invalid parameters for enforcer.")
 	}
@@ -62,60 +100,67 @@ func NewEnforcer(params ...interface{}) *Enforcer {
 func (e *Enforcer) InitWithFile(modelPath string, policyPath string) {
 	e.modelPath = modelPath
 
-	e.adapter = persist.NewFileAdapter(policyPath)
+	e.adapter = fileadapter.NewAdapter(policyPath)
 
 	e.enabled = true
+	e.autoSave = true
 
-	e.LoadModel()
-	e.LoadPolicy()
-}
-
-// InitWithDB initializes an enforcer with a model file and a policy from database.
-func (e *Enforcer) InitWithDB(modelPath string, driverName string, dataSourceName string) {
-	e.modelPath = modelPath
-
-	e.adapter = persist.NewDBAdapter(driverName, dataSourceName)
-
-	e.enabled = true
-
-	e.LoadModel()
-	e.LoadPolicy()
-}
-
-// InitWithConfig initializes an enforcer with a configuration file, by default is casbin.conf.
-func (e *Enforcer) InitWithConfig(cfgPath string) {
-	cfg := config.LoadConfig(cfgPath)
-
-	e.modelPath = cfg.ModelPath
-
-	if cfg.PolicyBackend == "file" {
-		e.adapter = persist.NewFileAdapter(cfg.PolicyPath)
-	} else if cfg.PolicyBackend == "database" {
-		e.adapter = persist.NewDBAdapter(cfg.DBDriver, cfg.DBDataSource)
+	if e.modelPath != "" {
+		e.LoadModel()
+		e.LoadPolicy()
 	}
-
-	e.enabled = true
-
-	e.LoadModel()
-	e.LoadPolicy()
 }
 
-// InitWithAdapter initializes an enforcer with an adapter.
+// InitWithAdapter initializes an enforcer with a database adapter.
 func (e *Enforcer) InitWithAdapter(modelPath string, adapter persist.Adapter) {
 	e.modelPath = modelPath
 
 	e.adapter = adapter
 
 	e.enabled = true
+	e.autoSave = true
 
-	e.LoadModel()
-	e.LoadPolicy()
+	if e.modelPath != "" {
+		e.LoadModel()
+		e.LoadPolicy()
+	}
+}
+
+// InitWithModelAndAdapter initializes an enforcer with a model and a database adapter.
+func (e *Enforcer) InitWithModelAndAdapter(m model.Model, adapter persist.Adapter) {
+	e.modelPath = ""
+	e.adapter = adapter
+
+	e.model = m
+	e.model.PrintModel()
+	e.fm = model.LoadFunctionMap()
+
+	e.enabled = true
+	e.autoSave = true
+
+	if e.adapter != nil {
+		e.LoadPolicy()
+	}
+}
+
+// NewModel creates a model.
+func NewModel(text ...string) model.Model {
+	m := make(model.Model)
+
+	if len(text) == 1 {
+		m.LoadModelFromText(text[0])
+	} else if len(text) != 0 {
+		panic("Invalid parameters for model.")
+	}
+
+	return m
 }
 
 // LoadModel reloads the model from the model CONF file.
 // Because the policy is attached to a model, so the policy is invalidated and needs to be reloaded by calling LoadPolicy().
 func (e *Enforcer) LoadModel() {
-	e.model = model.LoadModel(e.modelPath)
+	e.model = NewModel()
+	e.model.LoadModel(e.modelPath)
 	e.model.PrintModel()
 	e.fm = model.LoadFunctionMap()
 }
@@ -125,33 +170,66 @@ func (e *Enforcer) GetModel() model.Model {
 	return e.model
 }
 
+// SetModel sets the current model.
+func (e *Enforcer) SetModel(model model.Model) {
+	e.model = model
+}
+
+// GetAdapter gets the current adapter.
+func (e *Enforcer) GetAdapter() persist.Adapter {
+	return e.adapter
+}
+
+// SetAdapter sets the current adapter.
+func (e *Enforcer) SetAdapter(adapter persist.Adapter) {
+	e.adapter = adapter
+}
+
+// SetRoleManager sets the constructor function for creating a RoleManager.
+func (e *Enforcer) SetRoleManager(rmc rbac.RoleManagerConstructor) {
+	e.rmc = rmc
+}
+
 // ClearPolicy clears all policy.
 func (e *Enforcer) ClearPolicy() {
 	e.model.ClearPolicy()
 }
 
 // LoadPolicy reloads the policy from file/database.
-func (e *Enforcer) LoadPolicy() {
+func (e *Enforcer) LoadPolicy() error {
 	e.model.ClearPolicy()
-	e.adapter.LoadPolicy(e.model)
+	err := e.adapter.LoadPolicy(e.model)
+	if err != nil {
+		return err
+	}
 
 	e.model.PrintPolicy()
-
-	e.model.BuildRoleLinks()
+	e.model.BuildRoleLinks(e.rmc)
+	return nil
 }
 
-// SavePolicy saves the current policy (usually after changed with casbin API) back to file/database.
-func (e *Enforcer) SavePolicy() {
-	e.adapter.SavePolicy(e.model)
+// SavePolicy saves the current policy (usually after changed with Casbin API) back to file/database.
+func (e *Enforcer) SavePolicy() error {
+	return e.adapter.SavePolicy(e.model)
 }
 
-// Enable changes the enforcing state of casbin, when casbin is disabled, all access will be allowed by the Enforce() function.
-func (e *Enforcer) Enable(enable bool) {
+// EnableEnforce changes the enforcing state of Casbin, when Casbin is disabled, all access will be allowed by the Enforce() function.
+func (e *Enforcer) EnableEnforce(enable bool) {
 	e.enabled = enable
 }
 
+// EnableLog changes whether to print Casbin log to the standard output.
+func (e *Enforcer) EnableLog(enable bool) {
+	util.EnableLog = enable
+}
+
+// EnableAutoSave controls whether to save a policy rule automatically to the adapter when it is added or removed.
+func (e *Enforcer) EnableAutoSave(autoSave bool) {
+	e.autoSave = autoSave
+}
+
 // Enforce decides whether a "subject" can access a "object" with the operation "action", input parameters are usually: (sub, obj, act).
-func (e *Enforcer) Enforce(rvals ...string) bool {
+func (e *Enforcer) Enforce(rvals ...interface{}) bool {
 	if !e.enabled {
 		return true
 	}
@@ -170,29 +248,36 @@ func (e *Enforcer) Enforce(rvals ...string) bool {
 		for key, ast := range e.model["g"] {
 			rm := ast.RM
 			functions[key] = func(args ...interface{}) (interface{}, error) {
+				if rm == nil {
+					name1 := args[0].(string)
+					name2 := args[1].(string)
+
+					return name1 == name2, nil
+				}
+
 				if len(args) == 2 {
 					name1 := args[0].(string)
 					name2 := args[1].(string)
 
 					return (bool)(rm.HasLink(name1, name2)), nil
-				} else {
-					name1 := args[0].(string)
-					name2 := args[1].(string)
-					domain := args[2].(string)
-
-					return (bool)(rm.HasLink(name1, name2, domain)), nil
 				}
+
+				name1 := args[0].(string)
+				name2 := args[1].(string)
+				domain := args[2].(string)
+
+				return (bool)(rm.HasLink(name1, name2, domain)), nil
 			}
 		}
 	}
 	expression, _ = govaluate.NewEvaluableExpressionWithFunctions(expString, functions)
 
-	var policyResults []bool
+	var policyResults []Effect
 	if len(e.model["p"]["p"].Policy) != 0 {
-		policyResults = make([]bool, len(e.model["p"]["p"].Policy))
+		policyResults = make([]Effect, len(e.model["p"]["p"].Policy))
 
 		for i, pvals := range e.model["p"]["p"].Policy {
-			//log.Print("Policy Rule: ", pvals)
+			// util.LogPrint("Policy Rule: ", pvals)
 
 			parameters := make(map[string]interface{}, 8)
 			for j, token := range e.model["r"]["r"].Tokens {
@@ -203,16 +288,35 @@ func (e *Enforcer) Enforce(rvals ...string) bool {
 			}
 
 			result, err := expression.Evaluate(parameters)
-			//log.Print("Result: ", result)
+			// util.LogPrint("Result: ", result)
 
 			if err != nil {
-				policyResults[i] = false
+				policyResults[i] = EffectIndeterminate
+				panic(err)
 			} else {
-				policyResults[i] = result.(bool)
+				if !result.(bool) {
+					policyResults[i] = EffectIndeterminate
+				} else {
+					if effect, ok := parameters["p_eft"]; ok {
+						if effect == "allow" {
+							policyResults[i] = EffectAllow
+						} else if effect == "deny" {
+							policyResults[i] = EffectDeny
+						} else {
+							policyResults[i] = EffectIndeterminate
+						}
+					} else {
+						policyResults[i] = EffectAllow
+					}
+
+					if e.model["e"]["e"].Value == "priority(p_eft) || deny" {
+						break
+					}
+				}
 			}
 		}
 	} else {
-		policyResults = make([]bool, 1)
+		policyResults = make([]Effect, 1)
 
 		parameters := make(map[string]interface{}, 8)
 		for j, token := range e.model["r"]["r"].Tokens {
@@ -223,29 +327,73 @@ func (e *Enforcer) Enforce(rvals ...string) bool {
 		}
 
 		result, err := expression.Evaluate(parameters)
-		//log.Print("Result: ", result)
+		// util.LogPrint("Result: ", result)
 
 		if err != nil {
-			policyResults[0] = false
+			policyResults[0] = EffectIndeterminate
+			panic(err)
 		} else {
-			policyResults[0] = result.(bool)
+			if result.(bool) {
+				policyResults[0] = EffectAllow
+			} else {
+				policyResults[0] = EffectIndeterminate
+			}
 		}
 	}
 
-	//log.Print("Rule Results: ", policyResults)
+	// util.LogPrint("Rule Results: ", policyResults)
 
 	result := false
 	if e.model["e"]["e"].Value == "some(where (p_eft == allow))" {
 		result = false
-		for _, res := range policyResults {
-			if res {
+		for _, eft := range policyResults {
+			if eft == EffectAllow {
 				result = true
+				break
+			}
+		}
+	} else if e.model["e"]["e"].Value == "!some(where (p_eft == deny))" {
+		result = true
+		for _, eft := range policyResults {
+			if eft == EffectDeny {
+				result = false
+				break
+			}
+		}
+	} else if e.model["e"]["e"].Value == "some(where (p_eft == allow)) && !some(where (p_eft == deny))" {
+		result = false
+		for _, eft := range policyResults {
+			if eft == EffectAllow {
+				result = true
+			} else if eft == EffectDeny {
+				result = false
+				break
+			}
+		}
+	} else if e.model["e"]["e"].Value == "priority(p_eft) || deny" {
+		result = false
+		for _, eft := range policyResults {
+			if eft != EffectIndeterminate {
+				if eft == EffectAllow {
+					result = true
+				} else {
+					result = false
+				}
 				break
 			}
 		}
 	}
 
-	log.Print("Request ", rvals, ": ", result)
+	reqStr := "Request: "
+	for i, rval := range rvals {
+		if i != len(rvals)-1 {
+			reqStr += fmt.Sprintf("%v, ", rval)
+		} else {
+			reqStr += fmt.Sprintf("%v", rval)
+		}
+	}
+	reqStr += fmt.Sprintf(" ---> %t", result)
+	util.LogPrint(reqStr)
 
 	return result
 }
--- vendor/github.com/casbin/casbin/rbac_api.go
+++ vendor/github.com/casbin/casbin/rbac_api.go
@@ -14,24 +14,53 @@
 
 package casbin
 
-// GetRolesForUser gets roles for a user.
+// GetRolesForUser gets the roles that a user has.
 func (e *Enforcer) GetRolesForUser(name string) []string {
 	return e.model["g"]["g"].RM.GetRoles(name)
 }
 
+// GetUsersForRole gets the users that has a role.
+func (e *Enforcer) GetUsersForRole(name string) []string {
+	return e.model["g"]["g"].RM.GetUsers(name)
+}
+
+// HasRoleForUser determines whether a user has a role.
+func (e *Enforcer) HasRoleForUser(name string, role string) bool {
+	roles := e.GetRolesForUser(name)
+
+	hasRole := false
+	for _, r := range roles {
+		if r == role {
+			hasRole = true
+			break
+		}
+	}
+
+	return hasRole
+}
+
 // AddRoleForUser adds a role for a user.
-func (e *Enforcer) AddRoleForUser(user string, role string) {
-	e.AddGroupingPolicy([]string{user, role})
+// Returns false if the user already has the role (aka not affected).
+func (e *Enforcer) AddRoleForUser(user string, role string) bool {
+	return e.AddGroupingPolicy(user, role)
+}
+
+// DeleteRoleForUser deletes a role for a user.
+// Returns false if the user does not have the role (aka not affected).
+func (e *Enforcer) DeleteRoleForUser(user string, role string) bool {
+	return e.RemoveGroupingPolicy(user, role)
 }
 
 // DeleteRolesForUser deletes all roles for a user.
-func (e *Enforcer) DeleteRolesForUser(user string) {
-	e.RemoveFilteredGroupingPolicy(0, user)
+// Returns false if the user does not have any roles (aka not affected).
+func (e *Enforcer) DeleteRolesForUser(user string) bool {
+	return e.RemoveFilteredGroupingPolicy(0, user)
 }
 
 // DeleteUser deletes a user.
-func (e *Enforcer) DeleteUser(user string) {
-	e.RemoveFilteredGroupingPolicy(0, user)
+// Returns false if the user does not exist (aka not affected).
+func (e *Enforcer) DeleteUser(user string) bool {
+	return e.RemoveFilteredGroupingPolicy(0, user)
 }
 
 // DeleteRole deletes a role.
@@ -41,28 +70,56 @@ func (e *Enforcer) DeleteRole(role string) {
 }
 
 // DeletePermission deletes a permission.
-func (e *Enforcer) DeletePermission(permission string) {
-	e.RemoveFilteredPolicy(1, permission)
+// Returns false if the permission does not exist (aka not affected).
+func (e *Enforcer) DeletePermission(permission ...string) bool {
+	return e.RemoveFilteredPolicy(1, permission...)
 }
 
 // AddPermissionForUser adds a permission for a user or role.
-func (e *Enforcer) AddPermissionForUser(user string, permission string) {
-	e.AddPolicy([]string{user, permission})
+// Returns false if the user or role already has the permission (aka not affected).
+func (e *Enforcer) AddPermissionForUser(user string, permission ...string) bool {
+	params := make([]interface{}, 0, len(permission)+1)
+
+	params = append(params, user)
+	for _, perm := range permission {
+		params = append(params, perm)
+	}
+
+	return e.AddPolicy(params...)
+}
+
+// DeletePermissionForUser deletes a permission for a user or role.
+// Returns false if the user or role does not have the permission (aka not affected).
+func (e *Enforcer) DeletePermissionForUser(user string, permission ...string) bool {
+	params := make([]interface{}, 0, len(permission)+1)
+
+	params = append(params, user)
+	for _, perm := range permission {
+		params = append(params, perm)
+	}
+
+	return e.RemovePolicy(params...)
 }
 
 // DeletePermissionsForUser deletes permissions for a user or role.
-func (e *Enforcer) DeletePermissionsForUser(user string) {
-	e.RemoveFilteredPolicy(0, user)
+// Returns false if the user or role does not have any permissions (aka not affected).
+func (e *Enforcer) DeletePermissionsForUser(user string) bool {
+	return e.RemoveFilteredPolicy(0, user)
 }
 
 // GetPermissionsForUser gets permissions for a user or role.
-func (e *Enforcer) GetPermissionsForUser(user string) []string {
-	res := []string{}
+func (e *Enforcer) GetPermissionsForUser(user string) [][]string {
+	return e.GetFilteredPolicy(0, user)
+}
 
-	policy := e.GetFilteredPolicy(0, user)
-	for _, rule := range policy {
-		res = append(res, rule[1])
+// HasPermissionForUser determines whether a user has a permission.
+func (e *Enforcer) HasPermissionForUser(user string, permission ...string) bool {
+	params := make([]interface{}, 0, len(permission)+1)
+
+	params = append(params, user)
+	for _, perm := range permission {
+		params = append(params, perm)
 	}
 
-	return res
+	return e.HasPolicy(params...)
 }
