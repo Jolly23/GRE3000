97ca40bbaa9a99e41f4658fe304fcee5da090705
Mars <zhaolei@pm.me>
2026-03-15 14:55:57 -0400

    Update db.go

--- database/db.go
+++ database/db.go
@@ -9,8 +9,8 @@ import (
 
 const (
 	PgSslMode     = "disable"
-	PgHostAddress = "link-docker-postgresql"
-	PgHostPort    = "5432"
+	PgHostAddress = "localhost"
+	PgHostPort    = "53501"
 	DatabaseName  = "GRE3000"
 	PgUserName    = "DAISY"
 	PgPassword    = "778899"
