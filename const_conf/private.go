2d45b273cb864dbe12b2ebc5f9cd181bf7b60ece
Mars <zhaolei@pm.me>
2026-03-15 14:45:22 -0400

    upgrade

---
+++ const_conf/private.go
@@ -0,0 +1,11 @@
+package const_conf
+
+const (
+	DatabaseType  = "postgres"
+	PgSslMode     = "disable"
+	PgHostAddress = "link-docker-postgresql"
+	PgHostPort    = "5432"
+	DatabaseName  = "GRE3000"
+	PgUserName    = "JOLLY"
+	PgPassword    = "200900"
+)
